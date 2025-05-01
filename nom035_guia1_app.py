d8799001d3dc40c7c042b48141f8524a0786c13a
LEAN-arch <jbautistamsc@gmail.com>
2025-04-30 20:43:38 -0700

    Update nom035_guia1_app.py

--- nom035_guia1_app.py
+++ nom035_guia1_app.py
@@ -15,7 +15,7 @@ from datetime import datetime
 st.set_page_config(page_title="🧠 NOM-035 Guia I", layout="centered")
 
 # Clave de acceso predeterminada
-ACCESS_KEY = "NOM035_ACCESS_2025"
+ACCESS_KEY = "NOM035G1"
 
 if "responses" not in st.session_state:
     st.session_state.responses = []
